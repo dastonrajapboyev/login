e367dbabdb7f96fe5f265b98bf7e7597d289b05d
RustamNur <136016020+RustamNur@users.noreply.github.com>
2023-09-07 15:47:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <div style={{display: "flex", justifyContent: "center", background:"#39ace7"}}>
-      {/* <Login /> */}
-      <SignUp />
-    </div>
   </React.StrictMode>
 );
